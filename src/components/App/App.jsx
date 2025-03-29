89e06768ffa555c25b6ef527ab6d52d862ffbb81
Eugen Alekseev <devilsbrother1@gmail.com>
2025-03-29 13:39:42 +0200

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import ContactForm from '../ContactForm/ContactForm';
+import SearchBox from '../SearchBox/SearchBox';
+import ContactList from '../ContactList/ContactList';
+import userContacts from '../../data/userContacts.json';
+import { getLocal, setLocal } from '../LocalStorage/LocalStorage';
+import Container from '../Container/Container';
+import s from './App.module.css';
+
+const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = getLocal('contacts');
+    return savedContacts && Object.keys(savedContacts).length > 0
+      ? savedContacts
+      : userContacts;
+  });
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    setLocal('contacts', contacts);
+  }, [contacts]);
+
+  const addContact = (newContact) => {
+    console.log(newContact);
+    setContacts([...contacts, newContact]);
+  };
+
+  const deleteUser = (userId) => {
+    setContacts((prevContacts) => {
+      return prevContacts.filter((contact) => contact.id !== userId);
+    });
+  };
+
+  const visibleContacts = contacts.filter((contact) => {
+    return (
+      contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+      contact.number.includes(filter)
+    );
+  });
+
+  return (
+    <ul className={s.list}>
+      <li className={s.itemSide}>
+        <h1 className={s.title}>Phonebook</h1>
+        <ContactForm addContact={addContact} />
+        <SearchBox value={filter} onFilter={setFilter} />
+      </li>
+      <li className={s.itemCenter}>
+        <ContactList userContacts={visibleContacts} deleteUser={deleteUser} />
+      </li>
+    </ul>
+  );
+};
+
+export default App;
